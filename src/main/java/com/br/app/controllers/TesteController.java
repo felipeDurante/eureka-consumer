4bd9f96f9c779806009f3e27b3fe9c3f6d4936a8
Luiz Felipe do Carmo  Durante <luiz.durante@triadsystems.com.br>
2019-08-30 16:00:10 -0300

    first commit

---
+++ src/main/java/com/br/app/controllers/TesteController.java
@@ -0,0 +1,24 @@
+package com.br.app.controllers;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.br.app.services.EurekaService;
+
+@RestController
+public class TesteController {
+
+	@Autowired
+	private EurekaService service;
+
+	@GetMapping(value = "/getServiceName/{serviceName}")
+	public URI coco(@PathVariable("serviceName") String serviceName) {
+
+		System.out.println(service.getInstance(serviceName));
+		return service.getInstance(serviceName);
+	}
+}
